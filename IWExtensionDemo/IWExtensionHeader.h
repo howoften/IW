0cf099b51f7413e6b89d8e40f705da83ec22002d
iWe <i.1214@yahoo.com>
2017-12-31 11:30:42 +0800

    v0.0.3 Update some details.

---
+++ IWExtensionDemo/IWExtensionHeader.h
@@ -0,0 +1,16 @@
+//
+//  IWExtensionHeader.h
+//  IWExtensionDemo
+//
+//  Created by iWe on 2017/12/31.
+//  Copyright © 2017年 iWe. All rights reserved.
+//
+
+#ifndef IWExtensionHeader_h
+#define IWExtensionHeader_h
+
+// Use in MD5 -> HDBDRequest.swift
+#import <CommonCrypto/CommonDigest.h>
+
+
+#endif /* IWExtensionHeader_h */
